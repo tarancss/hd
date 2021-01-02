067aa5ae830910d557b478d43db90b790559af11
Santiago Sales <santiman625@gmail.com>
2021-01-02 01:10:23 +0100

    add linter

--- hd.go
+++ hd.go
@@ -1,7 +1,8 @@
-// Package hd provides hierarchical deterministic wallet ("HD wallet") functionality according to BIP39, BIP32 and BIP44.
-// The initialization of the wallet requires a 64-byte seed. It is recommended to generate seeds using BIP39 out of a 24 word mnemonic and passphrase which are easy to remember.
+// Package hd provides hierarchical deterministic wallet (HD wallet) functionality according to BIP39, BIP32 and BIP44.
+// The initialization of the wallet requires a 64-byte seed. It is recommended to generate seeds using BIP39 out of a
+// 24 word mnemonic and passphrase which are easy to remember.
 // Once the HdWallet is initialized, you can easily generate any address.
-// For a full description of what a HD wallet is, please read here: https://en.bitcoinwiki.org/wiki/Deterministic_wallet
+// For a full description of what a HD wallet is, please read: https://en.bitcoinwiki.org/wiki/Deterministic_wallet
 package hd
 
 import (
@@ -9,6 +10,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha512"
 	"errors"
+	"fmt"
 	"math/big"
 
 	"github.com/btcsuite/btcd/btcec"
@@ -28,14 +30,16 @@ const (
 )
 
 var (
-	// ErrInvalidSeedLen, recommended seed length is 64-byte
-	ErrInvalidSeedLen error = errors.New("ErrInvalidSeedLen: length of seed is invalid")
-	// ErrUnusableSeed will be reported if the seed cannot be used
-	ErrUnusableSeed error = errors.New("ErrUnusableSeed: the master key cannot be used")
+	// ErrInternal wraps errors reported by deps.
+	ErrInternal error = errors.New("hd internal error")
+	// ErrInvalidSeedLen will be reported when setting a wrong length (recommended seed length is 64-byte).
+	ErrInvalidSeedLen error = errors.New("hd: length of seed is invalid")
+	// ErrUnusableSeed will be reported if the seed cannot be used.
+	ErrUnusableSeed error = errors.New("hd: the master key cannot be used")
 )
 
-// HdWallet is a composed type
-type HdWallet struct {
+// HdWallet is a composed type.
+type HdWallet struct { //nolint:golint // changing would break compatibility
 	*hdkeychain.ExtendedKey // HD wallet branch from which account/addresses are generated
 }
 
@@ -49,14 +53,20 @@ func Init(seed []byte) (*HdWallet, error) {
 	// generate a BIP44 and Ethereum branch
 	tmpW, err := master.Child(hardened + purpose)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w ", ErrInternal, err)
 	}
+
 	tmpW, err = tmpW.Child(hardened + coin)
-	return &HdWallet{tmpW}, err
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w ", ErrInternal, err)
+	}
+
+	return &HdWallet{ExtendedKey: tmpW}, nil
 }
 
 // Address generates an address for 'wallet', flg should be either external or change and address number.
-func (w *HdWallet) Address(wallet uint32, flg uint8, addrNum uint32) (addr, key []byte, prv ecdsa.PrivateKey, err error) {
+func (w *HdWallet) Address(wallet uint32, flg uint8, addrNum uint32) (
+	addr, key []byte, prv ecdsa.PrivateKey, err error) {
 	var tmpW *hdkeychain.ExtendedKey
 	// get account
 	tmpW, err = w.Child(hardened + wallet)
@@ -73,8 +83,10 @@ func (w *HdWallet) Address(wallet uint32, flg uint8, addrNum uint32) (addr, key 
 	if err != nil {
 		return
 	}
+
 	privateKey, _ := tmpW.ECPrivKey()
 	prv = ecdsa.PrivateKey(*privateKey)
+
 	return crypto.PubkeyToAddress(privateKey.PublicKey).Bytes(), crypto.FromECDSA(&prv), prv, nil
 }
 
@@ -88,7 +100,10 @@ func getHdMaster(seed []byte) (*hdkeychain.ExtendedKey, error) {
 	// First take the HMAC-SHA512 of the master key and the seed data:
 	//   I = HMAC-SHA512(Key = "Bitcoin seed", Data = S)
 	hmac512 := hmac.New(sha512.New, []byte("Bitcoin seed"))
-	hmac512.Write(seed)
+	if _, err := hmac512.Write(seed); err != nil {
+		return nil, fmt.Errorf("%s: %w ", ErrInternal, err)
+	}
+
 	lr := hmac512.Sum(nil)
 
 	// Split "I" into two 32-byte sequences where left is master secret key and right is master chain code
@@ -102,6 +117,7 @@ func getHdMaster(seed []byte) (*hdkeychain.ExtendedKey, error) {
 	}
 
 	parentFP := []byte{0x00, 0x00, 0x00, 0x00}
+
 	return hdkeychain.NewExtendedKey([]byte{0x04, 0x88, 0xad, 0xe4}, secretKey, chainCode,
 		parentFP, 0, 0, true), nil // starts with xprv -> {0x04, 0x88, 0xad, 0xe4}
 }
--- hd_test.go
+++ hd_test.go
@@ -9,8 +9,9 @@ import (
 )
 
 func TestHdWallet(t *testing.T) {
-	var seed []byte
-	seed, _ = hex.DecodeString("642ce4e20f09c9f4d285c2b336063eaafbe4cb06dece8134f3a64bdd8f8c0c24df73e1a2e7056359b6db61e179ff45e5ada51d14f07b30becb6d92b961d35df4") // seed has been generated with mnemonic "tuna song credit master earn feature dutch nurse yellow ship caution relief ten drip trip couch increase nominee salt drift nation oval exhaust baby" and passphrase "password"
+	// seed has been generated with mnemonic "tuna song credit master earn feature dutch nurse yellow ship caution
+	// relief ten drip trip couch increase nominee salt drift nation oval exhaust baby" and passphrase "password"
+	seed, _ := hex.DecodeString("642ce4e20f09c9f4d285c2b336063eaafbe4cb06dece8134f3a64bdd8f8c0c24df73e1a2e7056359b6db61e179ff45e5ada51d14f07b30becb6d92b961d35df4") //nolint:lll // seed literal is 128 digits
 
 	w, err := Init(seed)
 	if err != nil {
@@ -18,25 +19,29 @@ func TestHdWallet(t *testing.T) {
 	}
 
 	// We generate 3 addresses for wallet 2, hd.External, indices from 0 to 2.
-	var expected [][]string = [][]string{
-		{"0xD43E2870777916Ede1f5Cc43F14f8C0741e11f96", "0x735e6eec7fbd869aafa61e50921b101eebc1d6961b8019a76bcf27cade1304b7"},
-		{"0xF4cEFC8d1AfaA51d5A5E7f57d214B60429cA4378", "0xfa7d6a67439ec17e07c10f10a4a9007e46583b5219cb909c8b474398b7216917"},
-		{"0x8A1847459c5FCD66f0B29012a21A2D5A314Ef1D0", "0x99c59090d814b1a3eb2b1f1715e7e7a09cbf8a770495a9a7836fb02226f8fd44"},
-	}
-	var addr, key, addrExp, keyExp []byte
+	var (
+		expected [][]string = [][]string{
+			{"0xD43E2870777916Ede1f5Cc43F14f8C0741e11f96", "0x735e6eec7fbd869aafa61e50921b101eebc1d6961b8019a76bcf27cade1304b7"},
+			{"0xF4cEFC8d1AfaA51d5A5E7f57d214B60429cA4378", "0xfa7d6a67439ec17e07c10f10a4a9007e46583b5219cb909c8b474398b7216917"},
+			{"0x8A1847459c5FCD66f0B29012a21A2D5A314Ef1D0", "0x99c59090d814b1a3eb2b1f1715e7e7a09cbf8a770495a9a7836fb02226f8fd44"},
+		}
+		addr, key, addrExp, keyExp []byte
+	)
+
 	for i := uint32(0); i < uint32(3); i++ {
 		addr, key, _, err = w.Address(uint32(2), External, i)
 		if err != nil {
 			t.Errorf("Address %d :%e", i, err)
 		}
+
 		addrExp, _ = hex.DecodeString(expected[i][0][2:])
-		if bytes.Compare(addr, addrExp) != 0 {
+		if bytes.Compare(addr, addrExp) != 0 { //nolint:gosimple // check 0 and not false
 			t.Errorf("Address %d does not match. Got:%x, expected:%x", i, addr, addrExp)
 		}
+
 		keyExp, _ = hex.DecodeString(expected[i][1][2:])
-		if bytes.Compare(addr, addrExp) != 0 {
+		if bytes.Compare(key, keyExp) != 0 { //nolint:gosimple // check 0 and not false
 			t.Errorf("Key %d does not match. Got:%x, expected:%x", i, key, keyExp)
 		}
 	}
-	return
 }
